e1fa78eb20e7a1f6bc01ca2bc2e26ae3a4cbde6f
Александр Боярский <kykyllkan@gmail.com>
2025-06-18 12:03:50 +0300

    tasks

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,53 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class MyHouse {
+  private door: boolean = false;
+  private tenants: Person[] = [];
+
+  constructor(private key: Key) {}
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Door is open');
+    } else {
+      console.log('Wrong key!');
+    }
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.constructor.name} came in`);
+    } else {
+      console.log('Door is closed');
+    }
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
